2de76639de9d73e0698899b9b93c51b8380e015d
trontzal <trontzal@gmail.com>
2023-03-05 20:59:55 +0100

    inicio

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PortalVue from 'portal-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+createApp(App).use(store).use(router).mount('#app').use(PortalVue).use(bootstrapVue)
